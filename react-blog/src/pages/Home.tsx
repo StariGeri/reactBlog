fc8021ac684137496a906d653194c1caa0bd626a
StariGeri <sgergostarosta@gmail.com>
2023-04-05 13:35:15 +0200

    top-curve

--- react-blog/src/pages/Home.tsx
+++ react-blog/src/pages/Home.tsx
@@ -7,9 +7,11 @@ function Home() {
   return (
     <section className="homeSection">
       <Curve />
+      <h1 className="homeTitle">Latest Posts</h1>
       <div className="contentWrapper">
         <BlogPostCard title="Lorem Ipsum is simply dummy text of the printing and typesetting industry." date="2020-01-01" id={1} />
         <BlogPostCard title="Lorem Ipsum is simply dummy text of the printing and typesetting industry." date="2020-01-01" id={2} />
+        <BlogPostCard title="Lorem Ipsum is simply dummy text of the printing and typesetting industry." date="2020-01-01" id={3} />
       </div>
     </section>
   );
