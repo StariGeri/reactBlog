274737b94ea1991bb49f72614d98145cf86cfb52
StariGeri <sgergostarosta@gmail.com>
2023-04-05 20:28:11 +0200

    multiple styled button component

--- react-blog/src/components/BlogPostCard.tsx
+++ react-blog/src/components/BlogPostCard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom"; // react-router-dom is needed to use routing when clicking the button of the blog post card
 import "./BlogPostCard.css";
-import Button_basic from "./Button_basic";
+import Button from "./Button";
 
 interface Props {
   title: string;
@@ -17,7 +17,7 @@ const BlogPostCard: React.FC<Props> = ({ title, date, id }) => {
           <h2 className="blogTitle">{title}</h2>
           <p className="blogDate">{date}</p>
           <Link to={`/blog/${id}`}>
-            <Button_basic text="Read" />
+            <Button text="Read" isFilled={true} />
           </Link>
         </div>
       </div>
---
+++ react-blog/src/components/LoginForm.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./LoginForm.css";
+import Button from "./Button";
+
+function LoginForm() {
+  return (
+    <form className="loginForm">
+      <div className="loginSubWrapper">
+        <h2 className="loginSubtitle">Admin Login</h2>
+      </div>
+      <div className="loginInputWrapper">
+        <label htmlFor="username">Username</label>
+        <input type="username" name="username" id="username" />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+      </div>
+      <div className="loginButtonWrapper">
+        <Link to={"/admin"}>
+          <Button text="Login" />
+        </Link>
+      </div>
+    </form>
+  );
+}
+export default LoginForm;
