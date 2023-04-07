4015f5c20809f2a63985e977d3a563daeb81f86e
StariGeri <sgergostarosta@gmail.com>
2023-04-08 00:42:00 +0200

    render blog page from fetched data

--- react-blog/src/App.tsx
+++ react-blog/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 //import components
 import Navbar from "./components/Navbar";
@@ -13,21 +13,21 @@ import Blog from "./pages/Blog";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 
-const App:React.FC = () => {
+const App: React.FC = () => {
   return (
     <Router>
       <div className="App">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/Login" element={<Login/>}/>
-          <Route path="/Admin" element={<Admin/>}/>
-          <Route path="/New" element={<New/>}/>
-          <Route path="/Edit" element={<Edit/>}/>
-          <Route path="/Blog/:id" element={<Blog title="Lorem ipsum is fake" date="2020.01.10." id={1} content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."/>}/>
-          <Route path="*" element={<NotFound/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Admin" element={<Admin />} />
+          <Route path="/New" element={<New />} />
+          <Route path="/Edit" element={<Edit />} />
+          <Route path="/Blog/:id" element={<Blog />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </div>
     </Router>
   );
--- react-blog/src/pages/Blog.tsx
+++ react-blog/src/pages/Blog.tsx
@@ -1,22 +1,16 @@
 import React, { useState, useEffect } from "react";
-import "./pageStyles/Blog.css";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 import Button from "../components/Button";
 import CommentSection from "../components/CommentSection";
+import "./pageStyles/Blog.css";
 
+function Blog() {
+    //getting the id from the url we are on
+    const { id } = useParams<{ id: string }>();
 
-interface blogProps {
-    title: string;
-    date: string;
-    id: number;
-    content: string;
-}
-
-const Blog: React.FC<blogProps> = ({ title, date, id, content }) => {
-
-    //random image from picsum.photos
+    //getting a random image from picsum
     const [image, setImage] = useState<string>("");
-
     const fetchImage = async () => {
         const response = await fetch("https://picsum.photos/600/400");
         const data = response.url;
@@ -27,6 +21,30 @@ const Blog: React.FC<blogProps> = ({ title, date, id, content }) => {
         fetchImage();
     }, []);
 
+    //fetching the blog post data from the api using the id we fetched
+    const [fetchBlogPost, setFetchBlogPost] = useState<any>({});
+    const fetchBlogPostData = async () => {
+        try {
+            const response = await axios.get(
+                `https://futurioninterview2.azurewebsites.net/BlogPost/${id}`
+            );
+            setFetchBlogPost(response.data);
+            //console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchBlogPostData();
+    }, [id]);
+
+    //formatting the fetched createdAt date
+    const formatDate = (dateString: string) => {
+        const date = new Date(dateString);
+        return date.toLocaleDateString();
+    };
+
     return (
         <div className="blogSection">
             <div className="blogContainer">
@@ -36,12 +54,12 @@ const Blog: React.FC<blogProps> = ({ title, date, id, content }) => {
                     </Link>
                 </div>
                 <div className="blogImgWrapper">
-                    <img src={image} className="blogImg" />
+                    <img src={image} className="blogImg" alt="blog post" />
                 </div>
                 <div className="blogContent">
-                    <p className="blogPostDate">{date}</p>
-                    <h2 className="blogPostTitle">{title}</h2>
-                    <p className="blogText">{content}</p>
+                    <p className="blogPostDate">{formatDate(fetchBlogPost.createdAt)}</p>
+                    <h2 className="blogPostTitle">{fetchBlogPost.title}</h2>
+                    <p className="blogText">{fetchBlogPost.body}</p>
                 </div>
             </div>
             <CommentSection />
