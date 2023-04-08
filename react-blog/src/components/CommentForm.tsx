6447ce4cfc4d9211626dc0c502b5caf70f86ee36
StariGeri <sgergostarosta@gmail.com>
2023-04-08 21:49:21 +0200

    import cleanups

--- react-blog/src/components/CommentForm.tsx
+++ react-blog/src/components/CommentForm.tsx
@@ -1,6 +1,4 @@
-import React, { useState,useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState } from 'react';
 import './componentStyles/CommentForm.css';
 import Button from './Button';
 
--- react-blog/src/components/Footer.tsx
+++ react-blog/src/components/Footer.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import { Link } from "react-router-dom";
 import "./componentStyles/Footer.css";
 import Logo from "./Logo";
 
